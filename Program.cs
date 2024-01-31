96ffa9e39c28238426bc56f2afbe5b99953e09e1
Charlotte Griffin <charlottegriffin11@gmail.com>
2024-01-31 08:19:26 -0700

    Charlotte

--- Program.cs
+++ Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿// Group 2-3: Driver Class
+
+// Welcome the user to the game
+
+// Create a game board array to store the players’ choices
+
+// Ask each player in turn for their choice and update the game board array
+
+// Print the board by calling the method in the supporting class
+
+// Check for a winner by calling the method in the supporting class
+
+// Notify the players when a win has occurred and which player won the game
