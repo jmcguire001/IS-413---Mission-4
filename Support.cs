3806b3d203442c3cdddc61c24aecb4192caf1a47
jmcguire001 <120243972+jmcguire001@users.noreply.github.com>
2024-01-31 10:54:52 -0700

    Created win scenarios

--- Support.cs
+++ Support.cs
@@ -8,7 +8,7 @@ namespace IS_413___Mission_4
 {
     internal class Support
     {
-        public string PrintBoard(string[] board)
+        public string PrintBoard(string[] board) //Parameters to receive: board, player 1, player 2, who is X
         {
             string gameBoard =
                 $"   #   #   \n" +
@@ -23,7 +23,109 @@ namespace IS_413___Mission_4
                 $" {board[6]} # {board[7]} # {board[8]} \n" +
                 $"   #   #   ";
 
+            // This still needs to evaluate for the winner
             return gameBoard;
+        }
+
+        public string CheckWinner(string[] board)
+        {
+            string winner = "";
+
+            // Check for a winner
+            if (board[0] == board[1] && board[1] == board[2] && board[0] != " ")
+            {
+                if (board[0] == "x")
+                {
+                    winner = "Player 1";
+                }
+                else
+                {
+                    winner = "Player 2";
+                }
+            }
+            else if (board[3] == board[4] && board[4] == board[5] && board[3] != " ")
+            {
+                if (board[3] == "X")
+                {
+                    winner = "Player 1";
+                }
+                else
+                {
+                    winner = "Player 2";
+                }
+            }
+            else if (board[6] == board[7] && board[7] == board[8] && board[6] != " ")
+            {
+                if (board[6] == "X")
+                {
+                    winner = "Player 1";
+                }
+                else
+                {
+                    winner = "Player 2";
+                }
+            }
+            else if (board[0] == board[3] && board[3] == board[6] && board[0] != " ")
+            {
+                if (board[0] == "X")
+                {
+                    winner = "Player 1";
+                }
+                else
+                {
+                    winner = "Player 2";
+                }
+            }
+            else if (board[1] == board[4] && board[4] == board[7] && board[1] != " ")
+            {
+                if (board[1] == "X")
+                {
+                    winner = "Player 1";
+                }
+                else
+                {
+                    winner = "Player 2";
+                }
+            }
+            else if (board[2] == board[5] && board[5] == board[8] && board[2] != " ")
+            {
+                if (board[2] == "X")
+                {
+                    winner = "Player 1";
+                }
+                else
+                {
+                    winner = "Player 2";
+                }
+            }
+            else if (board[0] == board[4] && board[4] == board[8] && board[0] != " ")
+            {
+                if (board[0] == "X")
+                {
+                    winner = "Player 1";
+                }
+                else
+                {
+                    winner = "Player 2";
+                }
+            }
+            else if (board[2] == board[4] && board[4] == board[6] && board[2] != " ")
+            {
+                if (board[2] == "x")
+                {
+                    winner = "Player 1";
+                }
+                else
+                {
+                    winner = "Player 2";
+                }
+            }
+            else
+            {
+                winner = "";
+            }
+
+            return winner;
         }
 
         static void Main(string[] args)
