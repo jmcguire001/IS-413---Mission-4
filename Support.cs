20d4a8626a805a22a8847ae134a39603483d76bb
jmcguire001 <120243972+jmcguire001@users.noreply.github.com>
2024-02-02 16:16:49 -0700

    Updated win conditions once again

--- Support.cs
+++ Support.cs
@@ -8,8 +8,10 @@ namespace IS_413___Mission_4
 {
     internal class Support
     {
+        // Method to print the game board
         public string PrintBoard(string[] board) //Parameters to receive: board, player 1, player 2, who is X
         {
+            // Create the game board
             string gameBoard =
                 $"   #   #   \n" +
                 $" {board[0]} # {board[1]} # {board[2]} \n" +
@@ -27,10 +29,13 @@ namespace IS_413___Mission_4
             return gameBoard;
         }
 
+        // Method to check for a winner
         public string CheckWinner(string[] board, string player1Symbol, string player1Name, string player2Name)
         {
+            // Initialize variables
             string winner = "";
             string Player1Symbol = player1Symbol;
+
             // Check for a winner
             if (board[0] == board[1] && board[1] == board[2] && board[0] != " ")
             {
@@ -120,6 +125,16 @@ namespace IS_413___Mission_4
                     winner = $"{player2Name} wins!";
                 }
             }
+            // Check for a cat's game or whether to continue
+            else
+            {
+                if (board[0] != "1" && board[1] != "2" && board[2] != "3" && board[3] != "4" && board[4] != "5" && board[5] != "6" && board[6] != "7" && board[7] != "8" && board[8] != "9")
+                {
+                    winner = "Cat's game! No winner.";
+                }
+            }
+
+            return winner;
         }
     }
 }
